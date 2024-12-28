f95501b1009f8a2422885701bc9d21af1358bd73
Wave Dev <66224387+wavedevgit@users.noreply.github.com>
2024-12-28 14:55:19 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const fs = require('fs/promises');
+const baseUrl = 'https://www.nytimes.com/svc/wordle/v2/';
+const addZeroToStartIfNeeded = (n) => (n < 10 ? '0' + String(n) : String(n));
+const getFileName = () => {
+    const d = date ? new Date(date) : new Date();
+    const month = d.getMonth() + 1;
+    return `${d.getFullYear()}-${addZeroToStartIfNeeded(month)}-${addZeroToStartIfNeeded(d.getDate())}.json`;
+};
+const getResult = async (url) => await (await fetch(url)).text();
+const doDate = async () => {
+    const result = await getResult(baseUrl.concat(getFileName()));
+    if (!result.includes('404')) await fs.writeFile('./answers/' + getFileName(), result, 'utf-8');
+};
+const main = async () => {
+    await doDate();
+};
+main();
